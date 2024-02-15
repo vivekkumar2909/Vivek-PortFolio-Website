5d770c627335e9e87e3c24e1c936b1f910c74dfc
vivekkumar2909 <rajvivek620065@gmail.com>
2024-02-15 22:28:03 +0530

    User Page

--- Portfolio-Vivek/src/components/User/User.jsx
+++ Portfolio-Vivek/src/components/User/User.jsx
@@ -8,24 +8,27 @@ import "./User.scss"
 function User() {
   return (
     <>
-        <div className='app_User-container'>
-            <div>
-                HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
 
-The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
-            </div>
-            <div>
-                HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
+      <div className='app_User-container'>
 
-The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
-            </div>
+
+        <div>
+          HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
+
+          The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
         </div>
+        <div>
+          HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
+
+          The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
+        </div>
+      </div>
     </>
   )
 }
 
 export default AppWrap(
-    MotionWrap(User, 'app__User'),
-    'coding',
-    'app__primarybg',
-  );
+  MotionWrap(User, 'app__User'),
+  'coding',
+  'app__primarybg',
+);
