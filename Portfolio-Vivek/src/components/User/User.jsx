d6abd4653f810ae3683e3cc3aece6b1593168dbd
vivekkumar2909 <rajvivek620065@gmail.com>
2024-02-16 18:01:29 +0530

    "Resume"

--- Portfolio-Vivek/src/components/User/User.jsx
+++ Portfolio-Vivek/src/components/User/User.jsx
@@ -6,23 +6,36 @@ import MotionWrap from '../../wrapper/MotionWrap';
 import "./User.scss"
 
 function User() {
+  // const val = 19;
   return (
     <>
+      <div>
 
-      <div className='app_User-container'>
+        <h1 className='user-heading'>
+          my coding profile
+        </h1>
 
+        <div className='app_User-container'>
+          <div className='box'>
+            When you add an image to a page using the HTML  element, the image will maintain the size and aspect ratio of the image file, or that of any HTML width or height attributes. Sometimes you would like the image to completely fill the box that you have placed it in. In that case you first need to decide what happens if the image is the wrong aspect ratio for the container.
 
-        <div>
-          HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
+            The image should completely fill the box, retaining aspect ratio, and cropping any excess on the side that is too big to fit.
+            The image should fit inside the box, with the background showing through as bars on the too-small side.
+            The image should fill the box and stretch, which may mean it displays at the wrong aspect ratio.
+            The object-fit property makes each of these approaches possible. In the example below you can see how different values of object-fit work when using the same image. Select the approach that works best for your design.
+          </div>
+          <div className='box'>
+            When you add an image to a page using the HTML  element, the image will maintain the size and aspect ratio of the image file, or that of any HTML width or height attributes. Sometimes you would like the image to completely fill the box that you have placed it in. In that case you first need to decide what happens if the image is the wrong aspect ratio for the container.
 
-          The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
-        </div>
-        <div>
-          HelloThe World Wide Web—usually called the Web for short—is a collection of different websites you can access through the Internet. A website is made up of related text, images, and other resources. Websites can resemble other forms of media—like newspaper articles or television programs—or they can be interactive in a way that's unique to computers.
+            The image should completely fill the box, retaining aspect ratio, and cropping any excess on the side that is too big to fit.
+            The image should fit inside the box, with the background showing through as bars on the too-small side.
+            The image should fill the box and stretch, which may mean it displays at the wrong aspect ratio.
+            The object-fit property makes each of these approaches possible. In the example below you can see how different values of object-fit work when using the same image. Select the approach that works best for your design.
+          </div>
 
-          The purpose of a website can be almost anything: a news platform, an advertisement, an online library, a forum for sharing images, or an educational site like us!
         </div>
       </div>
+
     </>
   )
 }
